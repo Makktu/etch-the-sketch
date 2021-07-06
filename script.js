14118155b261248e1af3195f33ef5f8df19fd0fb
Makktu <johnmcna@gmail.com>
2021-07-06 16:40:04 +0100

    made the grid

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const container = document.querySelector(".container");
+const gridContainer = document.querySelector(".grid-container");
+
+// let howManySquares = prompt("How many?");
+
+let howManySquares = 256;
+
+for (let j = 0; j < howManySquares; j++) {
+    const div = document.createElement("div");
+    div.classList.add("grid-square");
+    div.addEventListener("mouseover", function (e) {
+        e.target.style.backgroundColor = "white";
+    });
+    gridContainer.appendChild(div);
+}
+
+// TODO __________________________________________________
+// TODO current task:
+// TODO figure out how to display the grid (basic 4x4)
+// TODO __________________________________________________
